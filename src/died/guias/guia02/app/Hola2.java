0032d5e27929c8c85d32c445ffb0f70d4e4b34a6
martin97peru <34900001+martin97peru@users.noreply.github.com>
2018-04-10 10:42:03 -0300

    version inicial

---
+++ src/died/guias/guia02/app/Hola2.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package died.guias.guia02.app;
+
+/**
+ * @author Martin
+ *
+ */
+public class Hola2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("el tiempo actual en milis es "+ System.currentTimeMillis());
+	}
+}
